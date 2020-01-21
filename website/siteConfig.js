4a3090787106839eb6ee888c020f135791855146
Matt de Haast <matt.dehaast@coil.com>
2020-01-21 14:53:44 +0200

    attempt to fix site config

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,8 +23,8 @@
 const siteConfig = {
   title: 'Open Payments',
   tagline: 'A protocol for setting up payments between wallets',
-  url: 'https://openpayments.dev',
-  baseUrl: '/',
+  url: 'https://https://adrian.hopebailie.com/',
+  baseUrl: '/open-payments',
   projectName: 'open-payments',
   organizationName: 'adrianhopebailie',
   // For no header links in the top nav bar -> headerLinks: [],
