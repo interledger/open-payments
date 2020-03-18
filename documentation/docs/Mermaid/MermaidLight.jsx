a82d2dfe59e345d736a0ff4c8ab70ad460d1e9a0
Cairin Miche <cairinmichie@gmail.com>
2020-03-18 15:10:13 +0200

    Fix diagrams on toggle dark mode.

---
+++ documentation/docs/Mermaid/MermaidLight.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import mermaid from "mermaid";
+
+const config = {
+  theme: 'default',
+  startOnLoad:true,
+  sequence:{
+    diagramMarginX:50,
+    diagramMarginY:10,
+    actorMargin:50,
+    width:150,
+    height:65,
+    boxMargin:10,
+    boxTextMargin:5,
+    noteMargin:10,
+    messageMargin:35,
+    mirrorActors:true,
+    bottomMarginAdj:1,
+    useMaxWidth:true,
+    rightAngles:false,
+    showSequenceNumbers:false,
+  },
+}
+
+export default class MermaidLight extends React.Component {
+  componentDidMount() {
+    mermaid.initialize(config)
+    mermaid.contentLoaded();
+  }
+  render() {
+    return <div className="mermaid">{this.props.chart}</div>;
+  }
+}
