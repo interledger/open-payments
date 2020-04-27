fea1e223ec6126bf445ec4e556edea2f5a7422e2
adrianhopebailie <adrian@hopebailie.com>
2020-04-27 10:34:47 +0200

    Terminology, overview Add terminology page Update overview

--- documentation/sidebars.js
+++ documentation/sidebars.js
@@ -1,9 +1,18 @@
 module.exports = {
   docs: {
-    Specification:
-        ['overview', 'discovery' ,'wallet-wallet', 'wallet-app',
-          'invoices', 'mandates', 'charges' , 'auth', 'identity', 'web-monetization'
-        ],
-    'Use Cases': ['checkout_push', 'recurring', 'p2p', '3ppi']
+    Specification: [
+      'overview',
+      'terminology',
+      'discovery',
+      'wallet-wallet',
+      'wallet-app',
+      'invoices',
+      'mandates',
+      'charges',
+      'auth',
+      'identity',
+      'web-monetization',
+    ],
+    'Use Cases': ['checkout_push', 'recurring', 'p2p', '3ppi'],
   },
-};
+}
