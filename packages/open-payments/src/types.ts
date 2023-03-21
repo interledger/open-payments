bb7c5d1c7a4c1ec1e91d4c070af4bab0bc652817
Max Kurapov <max@interledger.org>
2023-03-21 14:03:06 +0100

    chore: add openapi open-payments http-signature-utils libraries (#244)
    * chore(all): add openapi, open-payments and http-signature-utils libraries
    
    * chore(ci): fix action reference
    
    * chore(root): ignore landing from prettier for now
    
    * docs(root): prettify readme
    
    * chore(root): ignore landing project from lint and format
    
    * chore(landing): remove name from package.jsoin
    
    * chore(open-payments): fix types
    
    * chore(open-payments): fix types
    
    * chore(ci): simplify action
    
    * chore(open-payments): remove fetch-schemas script
    
    * chore(root): remove raphql-markdown
    
    * chore(root): clean up dependencies
    
    * chore(ci): rename CI step
    
    * chore: fix deps
    
    * chore: add husky commit message

---
+++ packages/open-payments/src/types.ts
@@ -0,0 +1,133 @@
+import {
+  components as RSComponents,
+  paths as RSPaths,
+  operations as RSOperations
+} from './openapi/generated/resource-server-types'
+import {
+  components as ASComponents,
+  paths as ASPaths,
+  operations as ASOperations
+} from './openapi/generated/auth-server-types'
+
+export const getRSPath = <P extends keyof RSPaths>(path: P): string =>
+  path as string
+
+export type IncomingPayment = RSComponents['schemas']['incoming-payment']
+export type IncomingPaymentWithConnection =
+  RSComponents['schemas']['incoming-payment-with-connection']
+export type IncomingPaymentWithConnectionUrl =
+  RSComponents['schemas']['incoming-payment-with-connection-url']
+export type CreateIncomingPaymentArgs =
+  RSOperations['create-incoming-payment']['requestBody']['content']['application/json']
+export type IncomingPaymentPaginationResult =
+  PaginationResult<IncomingPaymentWithConnectionUrl>
+export type ILPStreamConnection =
+  RSComponents['schemas']['ilp-stream-connection']
+export type OutgoingPayment = RSComponents['schemas']['outgoing-payment']
+export type CreateOutgoingPaymentArgs =
+  RSOperations['create-outgoing-payment']['requestBody']['content']['application/json']
+type PaginationResult<T> = {
+  pagination: RSComponents['schemas']['page-info']
+  result: T[]
+}
+export type OutgoingPaymentPaginationResult = PaginationResult<OutgoingPayment>
+export type ForwardPagination = Omit<
+  RSOperations['list-incoming-payments']['parameters']['query'],
+  'last'
+> & {
+  last?: never
+}
+export type BackwardPagination = Omit<
+  RSOperations['list-incoming-payments']['parameters']['query'],
+  'first'
+> & {
+  first?: never
+}
+export type PaginationArgs = ForwardPagination | BackwardPagination
+export type PaymentPointer = RSComponents['schemas']['payment-pointer']
+export type JWK = RSComponents['schemas']['json-web-key']
+export type JWKS = RSComponents['schemas']['json-web-key-set']
+export type Quote = RSComponents['schemas']['quote']
+type QuoteArgsBase = {
+  receiver: RSOperations['create-quote']['requestBody']['content']['application/json']['receiver']
+}
+type QuoteArgsWithSendAmount = QuoteArgsBase & {
+  sendAmount?: RSComponents['schemas']['quote']['sendAmount']
+  receiveAmount?: never
+}
+type QuoteArgsWithReceiveAmount = QuoteArgsBase & {
+  sendAmount?: never
+  receiveAmount?: RSComponents['schemas']['quote']['receiveAmount']
+}
+export type CreateQuoteArgs =
+  | QuoteArgsWithSendAmount
+  | QuoteArgsWithReceiveAmount
+
+export const getASPath = <P extends keyof ASPaths>(path: P): string =>
+  path as string
+export type NonInteractiveGrantRequest = {
+  access_token: ASOperations['post-request']['requestBody']['content']['application/json']['access_token']
+  client: ASOperations['post-request']['requestBody']['content']['application/json']['client']
+}
+export type Grant = {
+  access_token: ASComponents['schemas']['access_token']
+  continue: ASComponents['schemas']['continue']
+}
+export type GrantRequest = {
+  access_token: ASOperations['post-request']['requestBody']['content']['application/json']['access_token']
+  client: ASOperations['post-request']['requestBody']['content']['application/json']['client']
+  interact: ASOperations['post-request']['requestBody']['content']['application/json']['interact']
+}
+export type GrantContinuationRequest = {
+  interact_ref: ASOperations['post-continue']['requestBody']['content']['application/json']['interact_ref']
+}
+export type PendingGrant = {
+  interact: ASComponents['schemas']['interact-response']
+  continue: ASComponents['schemas']['continue']
+}
+export type AccessToken = {
+  access_token: ASComponents['schemas']['access_token']
+}
+export const isPendingGrant = (
+  grant: PendingGrant | Grant
+): grant is PendingGrant => !!(grant as PendingGrant).interact
+
+export type AccessIncomingActions =
+  ASComponents['schemas']['access-incoming']['actions']
+export type AccessOutgoingActions =
+  ASComponents['schemas']['access-outgoing']['actions']
+export type AccessQuoteActions =
+  ASComponents['schemas']['access-quote']['actions']
+
+export type AccessItem = ASComponents['schemas']['access-item']
+
+export type AccessType =
+  | ASComponents['schemas']['access-incoming']['type']
+  | ASComponents['schemas']['access-outgoing']['type']
+  | ASComponents['schemas']['access-quote']['type']
+
+export type AccessAction = (
+  | AccessIncomingActions
+  | AccessOutgoingActions
+  | AccessQuoteActions
+)[number]
+
+export const AccessType: {
+  [key in 'IncomingPayment' | 'OutgoingPayment' | 'Quote']: AccessType
+} = {
+  IncomingPayment: 'incoming-payment',
+  OutgoingPayment: 'outgoing-payment',
+  Quote: 'quote'
+}
+
+export const AccessAction: Record<
+  'Create' | 'Read' | 'ReadAll' | 'Complete' | 'List' | 'ListAll',
+  AccessAction
+> = Object.freeze({
+  Create: 'create',
+  Read: 'read',
+  ReadAll: 'read-all',
+  Complete: 'complete',
+  List: 'list',
+  ListAll: 'list-all'
+})
