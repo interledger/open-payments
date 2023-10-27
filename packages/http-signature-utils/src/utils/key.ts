d15c6b1809a12611f36f24b17b3094a597a40a6e
Max Kurapov <max@interledger.org>
2023-10-27 18:32:18 +0200

    chore(hsu): adding jsdoc

--- packages/http-signature-utils/src/utils/key.ts
+++ packages/http-signature-utils/src/utils/key.ts
@@ -1,6 +1,13 @@
 import * as crypto from 'crypto'
 import * as fs from 'fs'
 
+/**
+ * Loads a EdDSA-Ed25519 private key.
+ *
+ * @param keyFilePath - The file path of the private key.
+ * @returns The KeyObject of the loaded private key
+ *
+ */
 export function loadKey(keyFilePath: string): crypto.KeyObject {
   let fileBuffer
   try {
@@ -23,6 +30,13 @@ export function loadKey(keyFilePath: string): crypto.KeyObject {
   return key
 }
 
+/**
+ * Generates a EdDSA-Ed25519 private key, and saves it in the given directory.
+ *
+ * @param dir - The directory to use for saving the key
+ * @returns The KeyObject that was generated
+ *
+ */
 export function generateKey(dir: string): crypto.KeyObject {
   const keypair = crypto.generateKeyPairSync('ed25519')
   if (!fs.existsSync(dir)) {
@@ -35,10 +49,19 @@ export function generateKey(dir: string): crypto.KeyObject {
   return keypair.privateKey
 }
 
-export function loadOrGenerateKey(keyFile?: string): crypto.KeyObject {
-  if (keyFile) {
+/**
+ * Loads a EdDSA-Ed25519 private key. If a path to the key was not provided,
+ * or if there were any errors when trying to load the given key, a new key is generated and
+ * saved in a file.
+ *
+ * @param keyFilePath - The file path of the private key.
+ * @returns The KeyObject of the loaded or generated private key
+ *
+ */
+export function loadOrGenerateKey(keyFilePath?: string): crypto.KeyObject {
+  if (keyFilePath) {
     try {
-      return loadKey(keyFile)
+      return loadKey(keyFilePath)
     } catch {
       /* Could not load key, generating new one */
     }
@@ -48,6 +71,13 @@ export function loadOrGenerateKey(keyFile?: string): crypto.KeyObject {
   return generateKey(TMP_DIR)
 }
 
+/**
+ * Loads a Base64 encoded EdDSA-Ed25519 private key.
+ *
+ * @param keyFilePath - The file path of the private key.
+ * @returns the KeyObject of the loaded private key, or undefined if the key was not EdDSA-Ed25519
+ *
+ */
 export function loadBase64Key(base64Key: string): crypto.KeyObject | undefined {
   const privateKey = Buffer.from(base64Key, 'base64').toString('utf-8')
   const key = crypto.createPrivateKey(privateKey)
