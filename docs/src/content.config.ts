6dfda7d64cca91be1741f72c5428261952bd6709
Max Kurapov <max@interledger.org>
2025-08-11 17:43:38 +0200

    chore: remove versioning from docs (#667)
    * chore: remove starlight-version
    
    * chore: remove docs/1.0 folder

--- docs/src/content.config.ts
+++ docs/src/content.config.ts
@@ -1,10 +1,8 @@
 import { defineCollection } from 'astro:content'
 import { docsLoader } from '@astrojs/starlight/loaders'
 import { docsSchema, i18nSchema } from '@astrojs/starlight/schema'
-import { docsVersionsLoader } from 'starlight-versions/loader'
 
 export const collections = {
   docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
-  i18n: defineCollection({ type: 'data', schema: i18nSchema() }),
-  versions: defineCollection({ loader: docsVersionsLoader() })
+  i18n: defineCollection({ type: 'data', schema: i18nSchema() })
 }
