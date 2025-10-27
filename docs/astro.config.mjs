ffcc9d854025fd7daf63e1591e23de16440753fa
brad-dow <162852233+brad-dow@users.noreply.github.com>
2025-10-27 07:32:10 -0500

    docs: prettier
    ran prettier

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -325,7 +325,9 @@ export default defineConfig({
             },
             {
               label: 'Send a remittance payment with a fixed receive amount',
-              translations: { es: 'Enviar una remesa con un monto de recepción fijo' },
+              translations: {
+                es: 'Enviar una remesa con un monto de recepción fijo'
+              },
               link: '/guides/onetime-remittance-fixed-receive/'
             },
             {
