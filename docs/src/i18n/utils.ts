efda669f640006329cfb0d5fc8d7f66132b1b9e9
Jon M <>
2025-11-06 07:12:18 -0600

    fix: read language from url

--- docs/src/i18n/utils.ts
+++ docs/src/i18n/utils.ts
@@ -1,7 +1,7 @@
 import { ui, languages, defaultLang, routes } from "./ui";
 
 export function getLangFromUrl(url: URL) {
-  const [, , lang] = url.pathname.split("/");
+  const [, lang] = url.pathname.split("/");
   if (lang in ui) return lang as keyof typeof languages;
   return defaultLang;
 }
