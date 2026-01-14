266c690f97790c018fbc29c2f7af272e8ed19cbf
Adrian Boros <adiboros@hotmail.com>
2026-01-14 15:55:03 +0200

    feat(docs): remove php snippets used by ChunkedSnippet

--- snippets/php/incoming-payment/incoming-payment-get-public.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//@! start chunk 1 | title=Get incoming payment
-$incomingPayment = $opClient->incomingPayment()->get(
-    [
-        'url' => $INCOMING_PAYMENT_URL
-    ]
-);
-//@! end chunk 1
