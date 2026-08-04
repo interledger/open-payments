2ac79962bfe0856cf9619df4262f3827bd60bcee
Melissa Henderson <57110301+melissahenderson@users.noreply.github.com>
2026-08-04 09:10:07 -0400

    docs: updates based on feedback

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -436,13 +436,6 @@ Key terminology notes:
               collapsed: true,
               items: [
                 {
-                  label: 'Verify ownership of a wallet address',
-                  translations: {
-                    es: 'Verificar la propiedad de una dirección de billetera'
-                  },
-                  link: '/guides/verify-wallet-address-ownership/'
-                },
-                {
                   label: 'Accept a one-time payment for an online purchase',
                   translations: {
                     es: 'Aceptar un pago único por una compra en línea'
@@ -498,6 +491,13 @@ Key terminology notes:
                     es: 'Obtener una concesión de pago saliente para pagos futuros'
                   },
                   link: '/guides/outgoing-grant-future-payments/'
+                },
+                {
+                  label: 'Verify ownership of a wallet address',
+                  translations: {
+                    es: 'Verificar la propiedad de una dirección de billetera'
+                  },
+                  link: '/guides/verify-wallet-address-ownership/'
                 }
               ]
             }
